3f034a5973fcb0def61258c4b72d27f7b6c890f4
Kauancode12 <kakabranco39@gmail.com>
2025-03-11 14:41:03 +0000

    Loops aninhados adicionado

--- xadrez.c
+++ xadrez.c
@@ -3,15 +3,15 @@
 int main() {
 
     //Declara as variaveis;
-    int bispo = 1;
-    int torre = 1;
-    int rainha = 1;
+    int bispo = 1, torre = 1, rainha = 1, cavalo = 1;
 
     //for executa o printf até o valor do bispo passar de 5, ele aumenta de um em um utilizando o incremento;
     for (bispo; bispo <= 5; bispo++){
 
         printf("Cima, Direita\n");
     }
+
+    printf("\n");
 
     //while exibe o printf enquanto o valor da torre estiver menor ou igual a cinco, ele aumenta de um em um utilizando o incremento;
     while (torre <=5){
@@ -21,12 +21,28 @@ int main() {
         torre++;
     }
 
+    printf("\n");
+
     //do while exibe o printf enquanto rainha for menor que oito ou igual, rainha aumenta de um em um utilizando o incremento;
     do {
 
         printf("Esquerda\n");
         rainha++;
     } while (rainha <=8);
+
+    printf("\n");
+
+    //Loops aninhados para simular a movimentação do cavalo, for roda uma vez enquanto while executa completamente a cada uma execução do for
+    for (cavalo; cavalo <= 1; cavalo++) {
+
+        int i = 0;
+        while (i < 2){
+            printf("Baixo\n");
+            i++;
+        }
+        printf("Esquerda\n");
+        printf("\n");
+    }
     
     return 0;
 }
