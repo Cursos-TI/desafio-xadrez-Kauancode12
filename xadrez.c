9a500f7966b0a1accd30462dc0627df55aeb7de3
Kauancode12 <kakabranco39@gmail.com>
2025-03-12 15:10:23 +0000

    recursive adicionado

--- xadrez.c
+++ xadrez.c
@@ -1,48 +1,47 @@
 #include <stdio.h>
 
+void recursiveLoop1(int b) {
+    if (b > 0) {
+        printf("Cima, Direita\n");
+        recursiveLoop1(b - 1);
+    }
+}
+
+void recursiveLoop2(int t) {
+    if (t > 0) {
+        printf("Direita\n");
+        recursiveLoop2(t - 1);
+    }
+}
+
+void recursiveLoop3(int r) {
+    if (r > 0) {
+        printf("Direita\n");
+        recursiveLoop3(r - 1);
+    }
+}
+
 int main() {
 
     //Declara as variaveis;
-    int bispo = 1, torre = 1, rainha = 1, cavalo = 1;
+    int bispo = 5, torre = 5, rainha = 8;
 
-    //for executa o printf até o valor do bispo passar de 5, ele aumenta de um em um utilizando o incremento;
-    for (bispo; bispo <= 5; bispo++){
-
-        printf("Cima, Direita\n");
-    }
-
-    printf("\n");
-
-    //while exibe o printf enquanto o valor da torre estiver menor ou igual a cinco, ele aumenta de um em um utilizando o incremento;
-    while (torre <=5){
-
-        printf("Direita\n");
-
-        torre++;
-    }
-
-    printf("\n");
-
-    //do while exibe o printf enquanto rainha for menor que oito ou igual, rainha aumenta de um em um utilizando o incremento;
-    do {
-
-        printf("Esquerda\n");
-        rainha++;
-    } while (rainha <=8);
-
-    printf("\n");
-
-    //Loops aninhados para simular a movimentação do cavalo, for roda uma vez enquanto while executa completamente a cada uma execução do for
-    for (cavalo; cavalo <= 1; cavalo++) {
-
-        int i = 0;
-        while (i < 2){
-            printf("Baixo\n");
-            i++;
-        }
-        printf("Esquerda\n");
+    recursiveLoop1(bispo);
         printf("\n");
-    }
+
+    recursiveLoop2(torre);
+        printf("\n");
+
+    recursiveLoop3(rainha);
+        printf("\n");
+
+        for(int c = 1, cavalo = 2; cavalo > 0; cavalo--, c--) {
+           
+        }
+
+        
+
+  
     
     return 0;
 }
